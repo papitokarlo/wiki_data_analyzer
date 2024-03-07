de614ea6d64c6d1f14b8bd0d01e3aeb0f9916da1
zura <zura_begadze>
2024-03-08 00:45:15 +0400

    api key

--- wiki_generator/defaults/connection.py
+++ wiki_generator/defaults/connection.py
@@ -2,6 +2,6 @@ from openai import OpenAI
 
 
 # ai_secret_key = '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'
-client = OpenAI(api_key='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')
+client = OpenAI(api_key='api key here ')
 
 openai_connection = client
